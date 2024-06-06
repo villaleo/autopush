ad77c65cfedee795cb68a28a2994eaade5db079d
Leonardo Villalobos <villaleobos@gmail.com>
2024-06-06 09:37:12 -0700

    update script usage

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
-mount:
-	@sudo mv bin/autopush /usr/local/bin
-
 deps:
 	@/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	@brew install go
 
 build: deps
 	@go build -o bin/autopush main.go
+	@sudo mv bin/autopush /usr/local/bin
 
-run: build mount
+run: build
 	autopush
+
+verify:
+	autopush -h
--- main.go
+++ main.go
@@ -12,12 +12,12 @@ import (
 )
 
 var (
-	timeout = flag.Int("t", 60, "the timeout until the the script is"+
-		"re-ran. \"m\" is the default timeout strategy. use \"tstr\" to"+
+	timeout = flag.Int("t", 60, "the timeout until the the script is "+
+		"re-ran. \"m\" is the default timeout strategy. use \"tstr\" to "+
 		"specify a timeout strategy.")
-	timeoutStrategy = flag.String("tstr", "ms", "the timeout strategy (unit)."+
+	timeoutStrategy = flag.String("tstr", "ms", "the timeout strategy (unit). "+
 		"accepted values are \"ms\", \"s\", and \"m\".")
-	msg = flag.String("msg", "automated commit by autopush", "the commit"+
+	msg = flag.String("msg", "automated commit by autopush", "the commit "+
 		"message to supply when an automatic commit is made")
 
 	green = color.New(color.FgGreen).SprintFunc()
